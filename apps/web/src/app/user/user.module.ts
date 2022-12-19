0fbb2ec4b6ef73e378a724f5f04bf577684aac7e
Adrian Kunz <clashsoft@hotmail.com>
2022-12-19 17:07:07 +0100

    feat: Scaffold user module

---
+++ apps/web/src/app/user/user.module.ts
@@ -0,0 +1,10 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {UserListComponent} from './user-list/user-list.component';
+
+@NgModule({
+  declarations: [UserListComponent],
+  imports: [CommonModule],
+})
+export class UserModule {
+}
