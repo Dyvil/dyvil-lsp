494e21db0c013405ef0d366a25455056a8f35efa
Adrian Kunz <clashsoft@hotmail.com>
2022-12-20 13:26:53 +0100

    feat(web): User routing

--- apps/web/src/app/user/user-routing.module.ts
+++ apps/web/src/app/user/user-routing.module.ts
@@ -1,15 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {UserListComponent} from './user-list/user-list.component';
 
 const routes: Routes = [
-  {path: 'users', loadChildren: () => import('./user/user.module').then(m => m.UserModule)},
+  {path: '', component: UserListComponent},
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forChild(routes),
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
+export class UserRoutingModule {
 }
--- apps/web/src/app/user/user.module.ts
+++ apps/web/src/app/user/user.module.ts
@@ -1,10 +1,11 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {UserListComponent} from './user-list/user-list.component';
+import {UserRoutingModule} from './user-routing.module';
 
 @NgModule({
   declarations: [UserListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, UserRoutingModule],
 })
 export class UserModule {
 }
