f82892e6b3821fdf236ba569c14dc448284e500b
Adrian Kunz <clashsoft@hotmail.com>
2022-12-20 13:26:43 +0100

    feat(web): UserService

--- apps/web/src/app/user/user.service.ts
+++ apps/web/src/app/user/user.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import type {CreateUserDto, UserDto} from '@software-tools/types';
+import {environment} from '../../environments/environment';
+
+const baseUrl = environment.apiUrl + '/api/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
+
+  create(dto: CreateUserDto) {
+    return this.http.post<UserDto>(baseUrl, dto);
+  }
+
+  findAll() {
+    return this.http.get<UserDto[]>(baseUrl);
+  }
+
+  findOne(id: string) {
+    return this.http.get<UserDto>(`${baseUrl}/${id}`);
+  }
+
+  update(id: string, dto: CreateUserDto) {
+    return this.http.patch<UserDto>(`${baseUrl}/${id}`, dto);
+  }
+
+  remove(id: string) {
+    return this.http.delete<UserDto>(`${baseUrl}/${id}`);
+  }
 }
