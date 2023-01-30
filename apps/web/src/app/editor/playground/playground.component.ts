4db077eaa8a14f030b1c3662312ded50824cb874
Adrian Kunz <clashsoft@hotmail.com>
2023-01-30 22:31:44 +0100

    feat(frontend): Compile to JS in playground

--- apps/web/src/app/editor/playground/playground.component.ts
+++ apps/web/src/app/editor/playground/playground.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {debounceTime, Subject} from 'rxjs';
+import {EditorComponent} from '../editor/editor.component';
 
 @Component({
   selector: 'stc-playground',
@@ -6,10 +8,41 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./playground.component.scss'],
 })
 export class PlaygroundComponent implements OnInit {
+  @ViewChild('editor', {static: true}) editor: EditorComponent;
+
   code = '';
+  compiled = 'class Foo {}';
+  compile$ = new Subject<string>();
+
+  worker?: Worker;
 
   async ngOnInit() {
     const examplePath = '/assets/examples/Greeter.dyv';
     this.code = await fetch(examplePath).then(r => r.text());
+
+    this.compile$.pipe(
+      debounceTime(400),
+    ).subscribe(code => {
+      this.compile(code).then(c => this.compiled = c);
+    });
+
+    this.worker = new Worker(new URL('./playground.worker.ts', import.meta.url));
   }
+
+  async compile(compile: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      if (!this.worker) {
+        return;
+      }
+
+      this.worker.postMessage({compile});
+      this.worker.addEventListener('message', msg => {
+        const compiled = msg.data.compiled;
+        if (compiled) {
+          resolve(compiled);
+        }
+      });
+    });
+  }
+
 }
