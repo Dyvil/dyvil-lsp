e0e82bee0f950ab90641e5e0074b0f7e8e841ea3
Adrian Kunz <clashsoft@hotmail.com>
2023-02-01 15:48:35 +0100

    fix: Remove unused libs and apps

--- apps/web/src/app/app-routing.module.ts
+++ apps/web/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   {path: 'editor', loadChildren: () => import('./editor/editor.module').then(m => m.EditorModule)},
-  {path: 'users', loadChildren: () => import('./user/user.module').then(m => m.UserModule)},
   {path: '', pathMatch: 'full', redirectTo: 'editor'},
 ];
 
--- libs/types/src/lib/dto.ts
+++
@@ -1,5 +0,0 @@
-import {Types} from 'mongoose';
-
-export type DTO<T> = {
-  [P in keyof T]: T[P] extends Types.ObjectId ? string : T[P];
-} & { _id: string };
