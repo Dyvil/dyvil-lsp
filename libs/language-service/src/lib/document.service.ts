362f09824642473342ae3aefba63da2904ae664e
Adrian Kunz <a.kunz@uni-kassel.de>
2024-01-16 14:39:27 +0100

    feat(language-service): Load initial files

--- libs/language-service/src/lib/document.service.ts
+++ libs/language-service/src/lib/document.service.ts
@@ -3,6 +3,7 @@ import {TextDocumentChangeEvent, TextDocuments} from 'vscode-languageserver';
 import {TextDocument} from 'vscode-languageserver-textdocument';
 import {ConnectionService} from './connection.service';
 import * as fs from 'fs';
+import {glob} from 'glob';
 
 export class DocumentService {
   documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
@@ -19,20 +20,42 @@ export class DocumentService {
   }
 
   private async init() {
-    console.log('initial documents', this.documents.all().map(d => d.uri));
+    const progress = await this.connectionService.connection.window.createWorkDoneProgress();
+    progress.begin('Loading Dyvil workspace...');
+    const workspaceFolders = await this.connectionService.connection.workspace.getWorkspaceFolders();
 
-    // 1. parse all files
-    for (const doc of this.documents.all()) {
-      const unit = this.parse(doc);
-      this.astCache.set(doc.uri, unit);
+    // 1. collect all files
+    progress.report('Collecting files...');
+    const files: string[] = [];
+    for (let workspaceFolder of workspaceFolders ?? []) {
+      const folder = parseFileUri(workspaceFolder.uri);
+      if (folder) {
+        files.push(...await glob(`${folder}/**/*.dyv`));
+      }
     }
 
-    // 2. resolve all against global scope
+    // 2. parse all files
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      progress.report(i / files.length, `Reading ${file}...`);
+
+      const uri = `file://${file}`;
+      const text = await fs.promises.readFile(file, 'utf-8');
+      const doc = TextDocument.create(uri, 'dyvil', 0, text);
+      const unit = this.parse(doc);
+      this.astCache.set(uri, unit);
+    }
+
+    // 3. resolve all against global scope
+    let i = 0;
     for (const unit of this.astCache.values()) {
+      progress.report(i / files.length, `Resolving ${unit.path}...`);
       unit.resolve(this.globalScope);
       unit.link();
       unit.lint(this.globalScope);
     }
+
+    progress.done();
   }
 
   private parse(doc: TextDocument): CompilationUnit {
@@ -99,4 +122,9 @@ export class DocumentService {
     const uri = typeof uriOrDoc === 'string' ? uriOrDoc : uriOrDoc.uri;
     return this.astCache.get(uri);
   }
+}
+
+function parseFileUri(uri: string): string | undefined {
+  const prefix = 'file://';
+  return uri.startsWith(prefix) ? uri.slice(prefix.length) : undefined;
 }
