d8d58eb82f15a09569e018fa8b7586ad10e26aaa
Adrian Kunz <clashsoft@hotmail.com>
2024-02-05 23:40:37 +0100

    test(web): Add cypress test

--- apps/web-e2e/src/e2e/app.cy.ts
+++ apps/web-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,51 @@
-import { getGreeting } from '../support/app.po';
-
 describe('web', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => cy.visit('/', {
+    onBeforeLoad(win) {
+      cy.spy(win.console, 'error');
+    }
+  }));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should not error when typing example', () => {
+    // click on .monaco-mouse-cursor-text
+    cy.get('.view-lines.monaco-mouse-cursor-text').first().click();
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome web');
+    // press ctrl/cmd + a
+    cy.get('.view-lines.monaco-mouse-cursor-text').first().type('{ctrl}a{cmd}a{backspace}');
+
+    cy.get('.view-lines.monaco-mouse-cursor-text').first().type(`
+class Greeter {
+var name: string = "World"
+var count: int = 0
+
+init(name: string) {
+this.name = name
+}
+
+func greet(): void {
+// line comment
+var /* block comment */ greeting = "Hello, " + this.name + "!" // comment after
+this.greet(greeting)
+}
+
+/** Sends a greeting */
+func greet(greeting: string): string {
+println(greeting)
+while this.count < 10 {
+var newCount = this.count + 1
+this.count = newCount
+}
+if this.count == 10 {
+println("Count is 10")
+} else if (this.count == 20) {
+println("Count is 20")
+} else {
+println("Count is " + this.count)
+}
+}
+}
+`, {parseSpecialCharSequences: false, delay: 20});
+    cy.window().then(win => {
+      expect(win.console.error).not.to.be.called;
+    });
   });
 });
