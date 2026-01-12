89c97d97525ed875f9532c75ab5de38b847ba181
Lukas Kawollek <kawollek@uni-kassel.de>
2026-01-12 12:47:54 +0100

    feat(yjs): clashsoft remarks

--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3333/api',
-  yjsWebsocketUrl: 'wss://yjs.uni-kassel.dev/ws',
+  yjsWebsocketUrl: 'ws://localhost:1234/ws',
 };
