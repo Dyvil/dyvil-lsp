3591bced6b760167001dc06cfbc53b8e5cde472a
Lukas Kawollek <92578339+derLumu@users.noreply.github.com>
2026-01-12 15:42:32 +0100

    feat: Yjs CRDT support (#44)
    * feat(monaco): added yjs crdt support
    
    * feat(monaco): added yjs url in env
    
    * feat(yjs): Dockerfile and docker-compose
    
    * feat(yjs): import upsis
    
    * feat(yjs): wtf auto formatter
    
    * feat(yjs): helm setup
    
    * feat(yjs): formatting
    
    * feat(yjs): formatting
    
    * feat(yjs): explain, what text is set
    
    * feat(yjs): trailing new line
    
    * feat(yjs): pr remarks
    
    * feat(yjs): clashsoft remarks
    
    * feat(yjs): typo
    
    * feat(yjs): hiding yjs from dyvil.org
    
    * feat(yjs): ...
    
    * feat(yjs): removed buttons and made own yjs method
    
    * fix: PR remarks
    
    ---------
    
    Co-authored-by: Adrian Kunz <a.kunz@uni-kassel.de>

--- apps/web/src/app/editor/editor/editor.component.ts
+++ apps/web/src/app/editor/editor/editor.component.ts
@@ -3,6 +3,7 @@ import {
   Component,
   ElementRef,
   EventEmitter,
+  inject,
   Input,
   OnChanges,
   OnDestroy,
@@ -10,9 +11,15 @@ import {
   SimpleChanges,
   ViewChild,
 } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {editor} from 'monaco-editor/esm/vs/editor/editor.api';
 import {MonacoLanguageClient} from 'monaco-languageclient';
 import {BrowserMessageReader, BrowserMessageWriter} from 'vscode-languageserver-protocol/browser';
+import {MonacoBinding} from 'y-monaco';
+import {WebsocketProvider} from 'y-websocket';
+import {Doc as YDoc} from 'yjs';
+
+import {environment} from '../../../environments/environment';
 import {createLanguageClient, ready} from './monaco';
 
 @Component({
@@ -30,16 +37,21 @@ export class EditorComponent implements AfterViewInit, OnChanges, OnDestroy {
   @Output() codeChanged = new EventEmitter<string>();
   @Output() ready = new EventEmitter<void>();
 
+  private activatedRoute = inject(ActivatedRoute);
+
   worker?: Worker;
   editor?: editor.IStandaloneCodeEditor;
   lspClient?: MonacoLanguageClient;
+  yjsBinding?: MonacoBinding;
+  yjsDoc?: YDoc;
+  websocketProvider?: WebsocketProvider;
 
   async ngAfterViewInit() {
     await ready;
 
     const domElement = this.container.nativeElement;
     this.editor = editor.create(domElement, {
-      value: this.code,
+      value: '', // will be set by yjs in order: server -> local storage -> default code
       language: this.language,
       theme: 'vs-dark',
       glyphMargin: true,
@@ -47,7 +59,7 @@ export class EditorComponent implements AfterViewInit, OnChanges, OnDestroy {
         enabled: true,
       },
       automaticLayout: true,
-      "semanticHighlighting.enabled": true,
+      'semanticHighlighting.enabled': true,
     });
     this.editor.onDidChangeModelContent(() => {
       this.editor && this.codeChanged.next(this.editor.getValue());
@@ -61,9 +73,48 @@ export class EditorComponent implements AfterViewInit, OnChanges, OnDestroy {
       this.lspClient = createLanguageClient({reader, writer});
       this.lspClient.start();
       reader.onClose(() => this.lspClient?.stop());
+
+      const room = this.activatedRoute.snapshot.queryParamMap.get('room');
+      if (room) {
+        // Use Yjs collaborative editing
+        this.initYjs(room);
+      } else {
+        // No room specified, use default code
+        this.editor.setValue(this.code);
+      }
     }
 
     this.ready.next();
+  }
+
+  private initYjs(room: string) {
+    // connect to the room and get the doc
+    this.yjsDoc = new YDoc();
+    this.websocketProvider = new WebsocketProvider(
+      environment.yjsWebsocketUrl,
+      room,
+      this.yjsDoc
+    );
+    const text = this.yjsDoc.getText('monaco');
+
+    // Bind Yjs text to Monaco model, will overwrite model content
+    this.yjsBinding = new MonacoBinding(
+      text,
+      // editor model is non-null as editor is created before
+      this.editor!.getModel()!,
+      new Set([this.editor!]),
+      this.websocketProvider.awareness
+    );
+
+    // Will be fired only once, when sync is done
+    this.websocketProvider.on('sync', (isSynced) => {
+      // If Yjs document is empty, insert demo code
+      if (isSynced && text.toString().length === 0) {
+        this.yjsDoc?.transact(() => {
+          text.insert(0, this.code);
+        });
+      }
+    });
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -83,5 +134,8 @@ export class EditorComponent implements AfterViewInit, OnChanges, OnDestroy {
     this.editor?.dispose();
     await this.lspClient?.stop();
     this.worker?.terminate();
+    this.yjsBinding?.destroy();
+    this.yjsDoc?.destroy();
+    this.websocketProvider?.destroy();
   }
 }
--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: '/api',
+  yjsWebsocketUrl: 'wss://yjs.uni-kassel.dev/ws',
 };
